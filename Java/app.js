23ad078aa0b923963123bfadfc96f29db21c5000
Abdul Moiz <Abdul Moiz>
2017-03-11 00:07:09 +0500

    Javascript Tables

---
+++ Java/app.js
@@ -0,0 +1,17 @@
+    var n= confirm('Are u ready',n);
+    console.log(n);
+    var a= prompt('Enter the Number',a);
+    var b=1;
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    document.write('<h3>' + a + 'x'+ b + '=' + a*b++ + '</h3>' +  '<br>' );
+    
+
+   
